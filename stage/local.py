01b1611afb54ab076504df69838a320b523b613c
GuGuMur <2221533105@qq.com>
2024-06-02 14:04:57 +0800

    fix: varible usage mistake

--- stage/local.py
+++ stage/local.py
@@ -34,7 +34,7 @@ def cell_deal_token(data: dict) -> dict:
             trap_cell_format = trapsformat[str(result["装置名称"])]
             print(trap_cell_format)
             traptype = trap_cell_format["type"]
-            for i in trapsformat["params"].keys():
+            for i in trap_cell_format["params"].keys():
                 if i.startwith("装置技能"):
                     result.pop("装置技能")
                     break
